f555599baba98d1235cf90e24d611d6dc07ae8db
Aditya Shyamsundar Bhuran <adityabhuran240802@gmail.com>
2025-01-07 03:53:26 +0000

    Comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);   basic conversions 
+
+
+// console.log("2" > 1);  datatypes changed 
+// console.log("02" > 1);
+
+console.log(null > 0);    //sometime converts into nan or sometimes converts into null 
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === Strictly checks by converting datatypes.
+
+console.log("2" === 2);
