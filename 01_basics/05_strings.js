62bde403c9f27b8dd21d57978802d316573e6c96
Aditya Shyamsundar Bhuran <adityabhuran240802@gmail.com>
2025-01-15 18:20:15 +0000

    String Commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,45 @@
+const name = "Aditya Shyamsundar Bhuran "
+const repoCount = 50 
+
+// console.log(name + repoCount + "Value");   do not follow thissyntax its Old 
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); this is the right Way .
+
+const gameName = new String('Aditya')
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));  //t
+// console.log(gameName.indexOf('y'));  //4
+
+// const newString = gameName.substring(0, 4)  //Adit
+// console.log(newString);
+
+// const anotherString = gameName.slice(-4, 4)  //it    you can use negative values in Slice 
+// console.log(anotherString);
+
+// const newStringOne = "   Aditya  "        
+// console.log(newStringOne);       //Aditya
+// console.log(newStringOne.trim());   //Removes the Not needed Space.
+
+const url = "AdityaBhuran02@gmail.net"
+
+// console.log(url.replace('Bhuran', 'Kadam'));  //helps to replace content easily
+
+console.log(url.includes('Bhuran'))
+
+
+
+
+
+
+
+
+
+
+
+
